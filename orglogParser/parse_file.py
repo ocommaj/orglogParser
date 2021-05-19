c492f84fd5b579528e9d74cd1204cf9fbd9d5686
ocommaj <ocommaj@gmail.com>
2021-05-18 21:14:10 -0500

    pythonickier parser

--- orglogParser/parse_file.py
+++ orglogParser/parse_file.py
@@ -1,16 +1,8 @@
-
 def parse_file(path, search_string=""):
-    results = []
+    def _filter(log_entry):
+        if not search_string or search_string in log_entry:
+            return log_entry
+
     with open(path) as f:
-        lines = f.readlines()
-
-        for line in lines:
-            res = _check_search(line, search_string)
-            if res:
-                res = res.strip()
-                results.append(res)
-    return results
-
-def _check_search(log_message, search_string):
-    if not search_string or search_string in log_message:
-        return log_message
+        log_entry = f.readlines()
+        return [le.strip() for le in log_entry if _filter(le)]
